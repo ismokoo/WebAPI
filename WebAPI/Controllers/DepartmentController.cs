fbff79ca56096df2ea2a56a32564d5525f1f64af
Ismo Karhiaho <45411035+ismokoo@users.noreply.github.com>
2021-03-20 19:42:09 +0200

    GET, POST and PUT implemented

--- WebAPI/Controllers/DepartmentController.cs
+++ WebAPI/Controllers/DepartmentController.cs
@@ -30,5 +30,59 @@ namespace WebAPI.Controllers
             }
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                           insert into dbo.Departments 
+                           values ('" + dep.DepartmentName + @"')
+                           ";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added successfully";
+            }
+            catch (Exception ex)
+            {
+
+                return $"Failed to add. {ex.Message}";
+            }
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                           update dbo.Departments set DepartmentName = '" + dep.DepartmentName + @"'
+                               where DepartmentID = " + dep.DepartmentID + @"
+                           ";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated successfully";
+            }
+            catch (Exception ex)
+            {
+
+                return $"Failed to update. {ex.Message}";
+            }
+        }
     }
 }
--- WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/Controllers/EmployeeController.cs
@@ -32,5 +32,73 @@ namespace WebAPI.Controllers
             }
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
+
+        public string Post(Employee emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                           insert into dbo.Employees 
+                           (EmployeeName,
+                           Department,
+                           MailID,
+                           DOJ)
+                           values 
+                           (
+                           '" + emp.EmployeeName + @"'
+                           ,'" + emp.Department + @"'
+                           ,'" + emp.MailID + @"'
+                           ,'" + emp.DOJ + @"'
+                           )
+                           ";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added successfully";
+            }
+            catch (Exception ex)
+            {
+
+                return $"Failed to add. {ex.Message}";
+            }
+        }
+
+        public string Put(Employee emp)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+
+                string query = @"
+                           update dbo.Employees set 
+                               EmployeeName = '" + emp.EmployeeName + @"'
+                               , Department = '" + emp.Department + @"'
+                               , MailID = '" + emp.MailID + @"'
+                               , DOJ = '" + emp.DOJ + @"'
+                               where EmployeeID = " + emp.EmployeeID + @"
+                           ";
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated successfully";
+            }
+            catch (Exception ex)
+            {
+
+                return $"Failed to update. {ex.Message}";
+            }
+        }
     }
 }
