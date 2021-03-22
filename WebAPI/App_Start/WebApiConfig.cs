bcde81c69c1e0e01820eb7285a4686cebca22c96
Ismo Karhiaho <45411035+ismokoo@users.noreply.github.com>
2021-03-22 15:39:10 +0200

    The CORS problem solved

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Web.Http.Cors;
 
 namespace WebAPI
 {
@@ -24,6 +25,8 @@ namespace WebAPI
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
                 new MediaTypeHeaderValue("text/html"));
+
+            config.EnableCors(new EnableCorsAttribute("http://localhost:3000", "*", "*"));
         }
     }
 }
